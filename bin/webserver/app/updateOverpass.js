4d492e7a5b883ea475e57229df1c4c0803ed5490
Martynas M <muizysmartynas@gmail.com>
2022-12-06 16:19:08 +0200

    Reverse geocoding
    Finished improving Overpass API updater. Implemented function to find to which city an object belongs to.

--- bin/webserver/app/updateOverpass.js
+++ bin/webserver/app/updateOverpass.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocoder = require('local-reverse-geocoder')
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'admin',
@@ -9,8 +10,17 @@ const pool = new Pool({
 })
 
 const updateCities = async (req, res) => {
+  let data
+  let parsedData
+  data = await new Promise((resolve, reject) =>{
     request('https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(id:3600072596)-%3E.searchArea;(node[%22place%22=%22city%22](area.searchArea););out%20body;%3E;out%20skel%20qt;', async (error, response, data) => {
-    parsedData = await JSON.parse(data)
+    resolve(data)
+  })
+  })
+    request('https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(id:3600072596)-%3E.searchArea;(node[%22place%22=%22city%22](area.searchArea););out%20body;%3E;out%20skel%20qt;', async (error, response, data) => {
+    
+  })
+  parsedData = await JSON.parse(data)
     for(let i = 0; i < parsedData.elements.length - 1; i++){
         pool.query(`SELECT EXISTS(SELECT 1 FROM city WHERE id=${parsedData.elements[i].id})`, (error, response) => {
         if(response.rows[0].exists == false){
@@ -22,9 +32,12 @@ const updateCities = async (req, res) => {
         }
       })
     }
+    data = await new Promise((resolve, reject) => {
+      request('https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(id:3600072596)-%3E.searchArea;(node[%22place%22=%22town%22](area.searchArea););out%20body;%3E;out%20skel%20qt;', async (error, response, data) => {
+      resolve(data)
   })
-    request('https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(id:3600072596)-%3E.searchArea;(node[%22place%22=%22town%22](area.searchArea););out%20body;%3E;out%20skel%20qt;', async (error, response, data) => {
-      parsedData = await JSON.parse(data)
+    })
+  parsedData = await JSON.parse(data)
       for(let i = 0; i < parsedData.elements.length - 1; i++){
         pool.query(`SELECT EXISTS(SELECT 1 FROM city WHERE id=${parsedData.elements[i].id})`, (error, response) => {
           if(response.rows[0].exists == false){
@@ -41,81 +54,145 @@ const STATUS = {
       UPDATE: `Overpass API updated successfully`
     }
     res.send({STATUS})
-  })
 }
 
-const updateLeisure = (req, res) => {
+const updateLeisure = async (req, res) => {
+  let data
   let parsedData
-  request('https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(id:3600072596)-%3E.searchArea;(node[%22leisure%22](area.searchArea););out%20body;%3E;out%20skel%20qt;', async (error, response, data) => {
-    parsedData = await JSON.parse(data)
-    for(let i = 0; i < parsedData.elements.length - 1; i++){
-      pool.query(`SELECT EXISTS(SELECT 1 FROM place WHERE id=${parsedData.elements[i].id})`, (error, response) => {
-        if(response.rows[0].exists == false){
-          pool.query(`INSERT INTO place (id, name, opening_hours, phone, website, city, lat, lon, type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [parsedData.elements[i].id, parsedData.elements[i].tags.name, parsedData.elements[i].tags.opening_hours, parsedData.elements[i].tags.phone, parsedData.elements[i].tags.website, parsedData.elements[i].tags['addr:city'], parsedData.elements[i].lat, parsedData.elements[i].lon, parsedData.elements[i].tags.leisure], (error, results) => {
-            if (error) {
-              throw error
-            }
-          })
-        }
-      })
-    }
+  data = await new Promise((resolve, reject) => {
+    request('https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(id:3600072596)-%3E.searchArea;(node[%22tourism%22=%22museum%22](area.searchArea););out%20body;%3E;out%20skel%20qt;', async (error, response, data) => {
+    resolve(data)
   })
-  request('https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(id:3600072596)-%3E.searchArea;(node[%22tourism%22=%22museum%22](area.searchArea););out%20body;%3E;out%20skel%20qt;', async (error, response, data) => {
-    parsedData = await JSON.parse(data)
-    for(let i = 0; i < parsedData.elements.length - 1; i++){
-      pool.query(`SELECT EXISTS(SELECT 1 FROM place WHERE id=${parsedData.elements[i].id})`, async (error, response) => {
-        if(response.rows[0].exists == false){
-          pool.query(`INSERT INTO place (id, name, opening_hours, phone, website, city, lat, lon, type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [parsedData.elements[i].id, parsedData.elements[i].tags.name, parsedData.elements[i].tags.opening_hours, parsedData.elements[i].tags.phone, parsedData.elements[i].tags.website, parsedData.elements[i].tags['addr:city'], parsedData.elements[i].lat, parsedData.elements[i].lon, parsedData.elements[i].tags.leisure], (error, results) => {
-            if (error) {
-              throw error
-            }
-          })
-        }
+  })
+  parsedData = await JSON.parse(data)
+  await new Promise(async(resolve, reject) => {
+    for(let i = 0; i <= parsedData.elements.length - 1; i++){
+    parsedData.elements[i].tags['addr:city'] = await new Promise((resolve, reject) => {
+      var point = { latitude: parsedData.elements[i].lat, longitude: parsedData.elements[i].lon }
+      geocoder.lookUp(point, (err, res) => {
+      resolve(res[0][0].name)
       })
+    })
+    pool.query(`SELECT EXISTS(SELECT 1 FROM place WHERE id=${parsedData.elements[i].id})`, (error, response) => {
+      if(response.rows[0].exists == false){
+        pool.query(`INSERT INTO place (id, name, opening_hours, phone, website, city, lat, lon, type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [parsedData.elements[i].id, parsedData.elements[i].tags.name, parsedData.elements[i].tags.opening_hours, parsedData.elements[i].tags.phone, parsedData.elements[i].tags.website, parsedData.elements[i].tags['addr:city'], parsedData.elements[i].lat, parsedData.elements[i].lon, parsedData.elements[i].tags.tourism], (error, results) => {
+          if (error) {
+            throw error
+          }
+        })
+      }
+    })
+    if(i == parsedData.elements.length - 1){
+    resolve()
+  }
+  }
+  
+  })
+  
+  data = await new Promise((resolve, reject) => {
+    request('https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(id:3600072596)-%3E.searchArea;(node[%22leisure%22](area.searchArea););out%20body;%3E;out%20skel%20qt;', async (error, response, data) => {
+    resolve(data)
+  })
+  })
+  parsedData = await JSON.parse(data)
+  await new Promise(async (resolve, reject) => {
+    for(let i = 0; i <= parsedData.elements.length - 1; i++){
+    parsedData.elements[i].tags['addr:city'] = await new Promise((resolve, reject) => {
+      var point = { latitude: parsedData.elements[i].lat, longitude: parsedData.elements[i].lon }
+      geocoder.lookUp(point, (err, res) => {
+      resolve(res[0][0].name)
+      })
+    })
+    pool.query(`SELECT EXISTS(SELECT 1 FROM place WHERE id=${parsedData.elements[i].id})`, (error, response) => {
+      if(response.rows[0].exists == false){
+        pool.query(`INSERT INTO place (id, name, opening_hours, phone, website, city, lat, lon, type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [parsedData.elements[i].id, parsedData.elements[i].tags.name, parsedData.elements[i].tags.opening_hours, parsedData.elements[i].tags.phone, parsedData.elements[i].tags.website, parsedData.elements[i].tags['addr:city'], parsedData.elements[i].lat, parsedData.elements[i].lon, parsedData.elements[i].tags.leisure], (error, results) => {
+          if (error) {
+            throw error
+          }
+        })
+      }
+    })
+    if(i == parsedData.elements.length - 1){
+      resolve()
     }
+  }
+  })
 
 const STATUS = {
-    UPDATE: `Overpass API updated successfully`
-  }
-  res.send({STATUS})
-  })
+  UPDATE: `Overpass API updated successfully`
+}
+res.send({STATUS})
 }
 
-const updateRestaurant = (req, res) => {
+const updateRestaurant = async (req, res) => {
+  let data
   let parsedData
-  request('https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(id:3600072596)-%3E.searchArea;(node[%22amenity%22=%22restaurant%22](area.searchArea););out%20body;%3E;out%20skel%20qt;', async (error, response, data) => {
-    parsedData = await JSON.parse(data)
-    for(let i = 0; i < parsedData.elements.length - 1; i++){
-      pool.query(`SELECT EXISTS(SELECT 1 FROM place WHERE id=${parsedData.elements[i].id})`, (error, response) => {
-        if(response.rows[0].exists == false){
-          pool.query(`INSERT INTO place (id, name, opening_hours, phone, website, city, lat, lon, type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [parsedData.elements[i].id, parsedData.elements[i].tags.name, parsedData.elements[i].tags.opening_hours, parsedData.elements[i].tags.phone, parsedData.elements[i].tags.website, parsedData.elements[i].tags['addr:city'], parsedData.elements[i].lat, parsedData.elements[i].lon, parsedData.elements[i].tags.amenity], (error, results) => {
-            if (error) {
-              throw error
-            }
-          })
-        }
-      })
-    }
-  })
-  request('https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(id:3600072596)-%3E.searchArea;(node[%22amenity%22=%22fast_food%22](area.searchArea););out%20body;%3E;out%20skel%20qt;', async (error, response, data) => {
-    parsedData = await JSON.parse(data)
-    for(let i = 0; i < parsedData.elements.length - 1; i++){
-      pool.query(`SELECT EXISTS(SELECT 1 FROM place WHERE id=${parsedData.elements[i].id})`, (error, response) => {
-        if(response.rows[0].exists == false){
-          pool.query(`INSERT INTO place (id, name, opening_hours, phone, website, city, lat, lon, type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [parsedData.elements[i].id, parsedData.elements[i].tags.name, parsedData.elements[i].tags.opening_hours, parsedData.elements[i].tags.phone, parsedData.elements[i].tags.website, parsedData.elements[i].tags['addr:city'], parsedData.elements[i].lat, parsedData.elements[i].lon, parsedData.elements[i].tags.amenity], (error, results) => {
-            if (error) {
-              throw error
-            }
-          })
-        }
-      })
-    }
 
-const STATUS = {
-    UPDATE: `Overpass API updated successfully`
-  }
-  res.send({STATUS})
+  data = await new Promise((resolve, reject) => {
+    request('https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(id:3600072596)-%3E.searchArea;(node[%22amenity%22=%22restaurant%22](area.searchArea););out%20body;%3E;out%20skel%20qt;', async (error, response, data) => {
+    resolve(data)
   })
+  })
+  parsedData = await JSON.parse(data)
+  await new Promise(async (resolve, reject) => {
+    for(let i = 0; i <= parsedData.elements.length - 1; i++){
+    parsedData.elements[i].tags['addr:city'] = await new Promise((resolve, reject) => {
+      var point = { latitude: parsedData.elements[i].lat, longitude: parsedData.elements[i].lon }
+      geocoder.lookUp(point, (err, res) => {
+      resolve(res[0][0].name)
+      })
+    })
+
+    pool.query(`SELECT EXISTS(SELECT 1 FROM place WHERE id=${parsedData.elements[i].id})`, (error, response) => {
+      if(response.rows[0].exists == false){
+        pool.query(`INSERT INTO place (id, name, opening_hours, phone, website, city, lat, lon, type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [parsedData.elements[i].id, parsedData.elements[i].tags.name, parsedData.elements[i].tags.opening_hours, parsedData.elements[i].tags.phone, parsedData.elements[i].tags.website, parsedData.elements[i].tags['addr:city'], parsedData.elements[i].lat, parsedData.elements[i].lon, parsedData.elements[i].tags.amenity], (error, results) => {
+          if (error) {
+            throw error
+          }
+        })
+      }
+    })
+    if(i == parsedData.elements.length - 1){
+    resolve()
+  }
+  }
+  
+  })
+  
+  data = await new Promise((resolve, reject) => {
+    request('https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(id:3600072596)-%3E.searchArea;(node[%22amenity%22=%22fast_food%22](area.searchArea););out%20body;%3E;out%20skel%20qt;', async (error, response, data) => {
+    resolve(data)
+  })
+  })
+  parsedData = await JSON.parse(data)
+  await new Promise(async(resolve, reject) => {
+    for(let i = 0; i <= parsedData.elements.length - 1; i++){
+    parsedData.elements[i].tags['addr:city'] = await new Promise((resolve, reject) => {
+      var point = { latitude: parsedData.elements[i].lat, longitude: parsedData.elements[i].lon }
+      geocoder.lookUp(point, (err, res) => {
+      resolve(res[0][0].name)
+      })
+    })
+    pool.query(`SELECT EXISTS(SELECT 1 FROM place WHERE id=${parsedData.elements[i].id})`, (error, response) => {
+      if(response.rows[0].exists == false){
+        pool.query(`INSERT INTO place (id, name, opening_hours, phone, website, city, lat, lon, type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [parsedData.elements[i].id, parsedData.elements[i].tags.name, parsedData.elements[i].tags.opening_hours, parsedData.elements[i].tags.phone, parsedData.elements[i].tags.website, parsedData.elements[i].tags['addr:city'], parsedData.elements[i].lat, parsedData.elements[i].lon, parsedData.elements[i].tags.amenity], (error, results) => {
+          if (error) {
+            throw error
+          }
+        })
+      }
+    })
+    if(i == parsedData.elements.length - 1){
+    resolve()
+  }
+  }
+  
+  })
+  
+const STATUS = {
+  UPDATE: `Overpass API updated successfully`
+}
+res.send({STATUS})
 }
 
 module.exports = {
--- bin/webserver/server.js
+++ bin/webserver/server.js
@@ -17,18 +17,31 @@ app.use(
   })
 )
 
+//Authentication
 app.get('/user/login', limiter.limiter, user.login)
 app.get('/user/create', limiter.limiter2, user.createUser)
 app.get('/user/update', limiter.limiter2, user.updateUser)
 app.get('/user/delete', user.deleteUser)
+
+//Update Overpass API
 app.get('/update/overpassapi/cities', overpass.updateCities)
 app.get('/update/overpassapi/leisure', overpass.updateLeisure)
 app.get('/update/overpassapi/restaurants', overpass.updateRestaurant)
+
+//Test connections to APIs and database
 app.get('/testconnections', test.testConnections)
 app.get('/score', score.getScore)
+
+//Get any location from Overpass API
 app.get('/locations', api.getLocations)
+
+//Vilnius events API
 app.get('/events', api.getVilniusEvents)
+
+//Meteo API to get weather forecast
 app.get('/weather', api.getWeather)
+
+//Database tables
 app.get('/table', db.getTable)
 
 app.listen(port, () => {
